0fdee87751ed1bf89382aacf29ef270cd6806a8a
Richard-Degenne <richdeg2@gmail.com>
2015-03-12 19:08:41 +0100

    Empty data models

---
+++ Datacenter.hpp
@@ -0,0 +1,12 @@
+
+#ifndef _DATACENTER_H
+#define _DATACENTER_H
+
+#include <iostream>
+
+class Datacenter {
+
+public:
+	void parse(char* argv[]);
+};
+
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CC=g++
 CFLAGS=--std=c++11 -Wall
 LDFLAGS=
-SRCS=
+SRCS=Row.cpp Server.cpp Slot.cpp Datacenter.cpp
 OBJS=$(SRCS:.cpp=.o)
 
 all: main.out
